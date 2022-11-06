76bba64e15c0686d19febe3fb40c628ea8d2ef75
kunb <balazs.kun@idomsoft.hu>
2022-11-06 09:57:59 +0100

    midigateway test 1

---
+++ midigateway/src/test/java/hu/boga/musaic/midigateway/sequence/SequenceGatewayImplTest.java
@@ -0,0 +1,31 @@
+package hu.boga.musaic.midigateway.sequence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SequenceGatewayImplTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    void open() {
+
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void stop() {
+    }
+}
